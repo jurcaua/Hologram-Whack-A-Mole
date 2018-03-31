26ea6c48da7938a5c9d1ef46792c2755bb49005a
Alex <jurcaua@gmail.com>
2018-03-31 01:05:07 -0400

    primary prototyping, image targets are not being found well

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vuforia;
+
+public class UIManager : MonoBehaviour {
+
+	public void LoadGame() {
+        SceneManager.LoadScene("main");
+    }
+
+    public void GoToMainMenu() {
+        SceneManager.LoadScene("title");
+    }
+
+    public void GoToSettings() {
+        SceneManager.LoadScene("settings");
+    }
+}
